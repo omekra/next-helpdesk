1692a7a4461f885abad0beedb40dec29c1d93307
Fued Kraide <omekra@hotmail.com>
2023-08-24 09:47:24 +1000

    page metadata

--- app/(dashboard)/tickets/[id]/page.jsx
+++ app/(dashboard)/tickets/[id]/page.jsx
@@ -1,5 +1,17 @@
 import { notFound } from "next/navigation";
+
 export const dynamicParams = true;
+
+export async function generateMetadata({ params }) {
+  const id = params.id;
+
+  const res = await fetch(`http://localhost:4000/tickets/${id}`);
+  const ticket = await res.json();
+
+  return {
+    title: `Next Helpdesk | ${ticket.title}`,
+  };
+}
 
 export async function generateStaticParams() {
   const res = await fetch("http://localhost:4000/tickets");
