4d3f0294991c70db4e9e2505ecb7a364e3351a95
Fued Kraide <omekra@hotmail.com>
2023-08-24 11:21:33 +1000

    auth forms

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,7 +1,16 @@
+"use client";
+import AuthForm from "../AuthForm";
+
 export default function Signup() {
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+    console.log("user sign up", email, password);
+  };
+
   return (
     <main>
       <h2 className="text-center">Signup</h2>
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 }
