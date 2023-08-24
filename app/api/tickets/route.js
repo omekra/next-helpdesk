ccd7b24751a6846a4546911a7cc3c0316957b4c9
Fued Kraide <omekra@hotmail.com>
2023-08-24 10:15:34 +1000

    dynamic vs static route handlers

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -1,8 +1,9 @@
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const res = await fetch("http://localhost:4000/tickets");
-
   const tickets = await res.json();
 
   return NextResponse.json(tickets, {
