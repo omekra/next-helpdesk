68ac389c7bb586ed69e3a40954a529e42db02a38
Fued Kraide <omekra@hotmail.com>
2023-08-25 12:06:33 +1000

    fetching data from supabase

--- app/(dashboard)/tickets/TicketList.jsx
+++ app/(dashboard)/tickets/TicketList.jsx
@@ -1,15 +1,17 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 import Link from "next/link";
 
 async function getTickets() {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const supabase = createServerComponentClient({ cookies });
 
-  const res = await fetch("http://localhost:4000/tickets", {
-    next: {
-      revalidate: 0, // use 0 to opt out of using cache
-    },
-  });
+  const { data, error } = await supabase.from("tickets").select();
 
-  return res.json();
+  if (error) {
+    console.log("🚀 ~ error:", error.message);
+  }
+
+  return data;
 }
 
 export default async function TicketList() {
